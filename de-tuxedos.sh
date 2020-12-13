b727988c662bf22f94749ef7642092ff95cc4c56
Immanuel Albrecht <mail@immanuel-albrecht.de>
2020-12-13 07:55:05 +0100

    Add files via upload

---
+++ de-tuxedos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "remove all packages with tuxedo in their names..."
+
+sudo apt list --installed | grep tuxedo | tr '/' ' ' | awk '{ print $1 }' | xargs sudo apt -y purge
+
+echo "removing tuxedo repositories"
+sudo rm /etc/apt/sources.list.d/*tuxedo*.list
+
+echo "updating..."
+sudo apt -y update
+
+echo "running upgrade..."
+sudo apt -y upgrade
+
+echo "removing bloatware..."
+for pkg in libreoffice rhythmbox pidgin libreoffice-common minetest shotwell libreoffice-base libreoffice-draw libreoffice-writer libreoffice-calc freeciv freeciv-client-gtk freeciv-data freeciv-server; do
+	sudo apt -y remove $pkg
+done
+
+echo "removing unused packages"
+sudo apt -y autoremove
+
+
+
