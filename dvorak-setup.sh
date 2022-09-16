09fac7c067bb4dcda5ccb5ec1929a8a693664a04
Immanuel Albrecht <mail@immanuel-albrecht.de>
2022-09-16 11:10:53 +0200

    updated link

--- dvorak-setup.sh
+++ dvorak-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "customizing programmer's dvorak layout"
-sudo wget https://immanuel-albrecht.de/pub/usr-share-X11-xkb-symbols-us -O /usr/share/X11/xkb/symbols/us
+sudo wget https://immanuel-albrecht.de/pdvrkde/usr-share-X11-xkb-symbols-us -O /usr/share/X11/xkb/symbols/us
 
